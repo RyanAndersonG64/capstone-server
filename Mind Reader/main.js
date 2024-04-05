013a53e5c420573ff37c73e734574c0768f04726
RyanAndersonG64 <ryan.anderson.g64@gmail.com>
2024-04-05 09:40:59 -0400

    final version

--- Mind Reader/main.js
+++ Mind Reader/main.js
@@ -1,7 +1,8 @@
 //initial conditions, starts app on home screen with appropriate text and buttons
 let activeScreen = 0;
 updateButtons();
-document.querySelector(".symbolsList").style.visibility="hidden";
+createSymbolsList();
+document.querySelector("#symbolsList").style.visibility="hidden";
 
 function saveScreen(){ //stores active screen to be read if page is refreshed
         sessionStorage.setItem("refresh", "true");
@@ -19,9 +20,21 @@ window.onload = function() { //if page is refreshed, it loads the active screen 
 }
 
 //the active screen will display the corresponding strings from the following arrays; an empty string means that that page should have no text in that location
-let topTextArray = ['I can read your mind!', 'Pick a number from 0-99, inclusive', 'Add both digits together to get a new number', 'Subtract the second number from the first', 'Placeholder for bottom text position', "Your number's symbol is"];
-let bottomTextArray = ["Press GO to begin", "Once you've thought of a number, click NEXT", "For example: 14 --> 1 + 4 = 5 7 --> 0 + 7", "ex: 14 - 5 = 9", "Find the symbol corresponding to your new number", ""];
+let topTextArray = ['I can read your mind!', 'Choose a number from 0-99', 'Add both digits together to get a new number', 'Subtract the second number from the first', 'Placeholder for bottom text position', "Your number's symbol is"];
+let bottomTextArray = ["Press GO to begin", "Once you've thought of a number, click NEXT", "For example: 14 --> 1 + 4 = 5 7 --> 0 + 7", "For example: 14 - 5 = 9", "Find the symbol corresponding to your new number (scroll or use 'ctrl + F' to find large numbers", ""];
 let pageNumbersArray = ["1 of 6", "2 of 6", "3 of 6", "4 of 6", "5 of 6", "6 of 6"];
+
+function createSymbolsList(){ //generates list of numbers and symbols
+    let numbersArray = []; //creates an array with numbers 0-99 for generating list of numbers and corresponding symbols
+    for (let i = 0; i <= 99; i++) {
+        numbersArray[i] = i;
+    }
+    let symbolsArray = ["~\n", "!\n", "@\n", "#\n", "$\n", "%\n", "^\n", "&\n", "*\n"]; //creates array with symbols to match numbers with
+    for (i = numbersArray[0]; i <= numbersArray[numbersArray.length-1]; i++) {
+        let symbolForNumber = symbolsArray[numbersArray[i] % 9];
+        document.querySelector("#symbolsList").textContent += numbersArray[i] + " = " +  symbolForNumber;
+    }
+}
 
 function updateButtons(){ //changes button text and/or visibility when active screen changes
 if (activeScreen == 0 || activeScreen == 5) {
@@ -31,6 +44,8 @@ if (activeScreen == 0 || activeScreen == 5) {
 }
 if (activeScreen == 0) {
     document.getElementById("goResetButton").textContent="GO";
+} else if (activeScreen == 5) {
+    document.getElementById("goResetButton").textContent="PLAY AGAIN";
 } else {
     document.getElementById("goResetButton").textContent="RESET";
 }
@@ -59,9 +74,9 @@ function updateText() { //changes page and button text to that of the new active
 
 function showHideSymbols() { //displays list of symbols on the appropriate page and hides it on other pages
     if (activeScreen == 4) {
-        document.querySelector(".symbolsList").style.visibility="visible";
+        document.querySelector("#symbolsList").style.visibility="visible";
     } else {
-        document.querySelector(".symbolsList").style.visibility="hidden";
+        document.querySelector("#symbolsList").style.visibility="hidden";
     }
 }
 
--- Mind Reader/pseudocode.md
+++ Mind Reader/pseudocode.md
@@ -13,3 +13,8 @@ click event for 'next' button to move to next screen
         button = reset
 
 display symbols for each number
+
+generate list of symbols and numbers:
+    for loop to create array filled with numbers from 0 - 99
+    create array with symbols
+    function to create list items by combining numbers and symbols using %
